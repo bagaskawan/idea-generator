b3021493200f268672885b1732230b3421dbb04b
bagaskawan <bagaskawan@gmail.com>
2025-08-05 18:47:59 +0700

    change gemini api key

--- src/app/api/generate-idea/route.ts
+++ src/app/api/generate-idea/route.ts
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
 // Inisialisasi Gemini client dengan API Key dari environment variables
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY!);
 
 export async function POST(request: Request) {
   try {
