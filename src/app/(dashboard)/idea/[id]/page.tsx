c5c9f9f98d35ca083a47317710bd6386c670eacc
bagaskawan <bagaskawan@gmail.com>
2025-08-14 06:27:36 +0700

    fix IdeaDetailView

--- src/app/(dashboard)/idea/[id]/page.tsx
+++ src/app/(dashboard)/idea/[id]/page.tsx
@@ -1,14 +1,12 @@
 import IdeaDetailView from "@/components/idea/IdeaDetailView";
 import { FullScreenModalWrapper } from "@/components/ui/FullScreenModalWrapper";
 
-type PageProps = {
-  params: {
-    id: string;
-  };
-};
-
 // This is a Server Component
-export default function IdeaDetailPage({ params }: PageProps) {
+export default async function IdeaDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = params;
 
   // We pass the id to the client component that handles data fetching and state.
