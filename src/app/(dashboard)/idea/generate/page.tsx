f49fe2f205e198e8249f02838d43bda07c6dd857
bagaskawan <bagaskawan@gmail.com>
2025-08-18 09:45:19 +0700

    downgrade to v14

--- src/app/(dashboard)/idea/generate/page.tsx
+++ src/app/(dashboard)/idea/generate/page.tsx
@@ -1,23 +1,12 @@
-// src/app/(dashboard)/idea/new/page.tsx
 "use client";
 
 import AddIdeaForm from "@/components/forms/AddIdeaForm";
 import { FullScreenModalWrapper } from "@/components/ui/FullScreenModalWrapper";
-import { useIdeaManager } from "@/utils/idea/useIdeaManager";
-import { useRouter } from "next/navigation";
 
 export default function GenerateIdeaPage() {
-  const router = useRouter();
-  const { addItem } = useIdeaManager();
-
-  const handleAddIdea = async (title: string, description: string) => {
-    addItem(title, description);
-    router.back();
-  };
-
   return (
     <FullScreenModalWrapper>
-      <AddIdeaForm onSubmit={handleAddIdea} />
+      <AddIdeaForm />
     </FullScreenModalWrapper>
   );
 }
